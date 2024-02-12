dc402b39e4e72f290ae6f639cd7fb69cc5e007e0
anthony-mini <anthony-mini@outlook.fr>
2024-02-12 14:36:02 +0100

    Mise à jour de la classe Subject avec des commentaires en français expliquant l'utilisation des types génériques

--- dist/helpers/subject.js
+++ dist/helpers/subject.js
@@ -1,5 +1,6 @@
 export class Subject {
     constructor() {
+        // T est un principe de polymorphisme, cela permet de déclaré qu'une seul fois la classe et de la réutiliser avec des types différents (voir class Game ou l'on déclare un Subject<Cell>. T est donc Cell dans ce cas là. Cela permet de ne pas avoir à créer une classe SubjectCell, SubjectGame, SubjectPlayer, etc... Egalement T, U, V, etc... sont des conventions de nommage pour les types génériques. voir https://www.typescriptlang.org/docs/handbook/generics.html)
         this.listeners = [];
     }
     listen(listener) {
--- src/helpers/subject.ts
+++ src/helpers/subject.ts
@@ -1,4 +1,5 @@
 export class Subject<T> {
+  // T est un principe de polymorphisme, cela permet de déclaré qu'une seul fois la classe et de la réutiliser avec des types différents (voir class Game ou l'on déclare un Subject<Cell>. T est donc Cell dans ce cas là. Cela permet de ne pas avoir à créer une classe SubjectCell, SubjectGame, SubjectPlayer, etc... Egalement T, U, V, etc... sont des conventions de nommage pour les types génériques. voir https://www.typescriptlang.org/docs/handbook/generics.html)
   private listeners: ((t: T) => void)[] = [];
 
   listen(listener: (t: T) => void) {
